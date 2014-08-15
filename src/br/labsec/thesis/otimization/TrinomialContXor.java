0cc982dd7cb36ddac4735721321a545baea5f203
gbanegas <g.banegas@inf.ufsc.br>
2014-08-15 11:08:55 -0300

    Add the first row of the xls

--- src/br/labsec/thesis/otimization/TrinomialContXor.java
+++ src/br/labsec/thesis/otimization/TrinomialContXor.java
@@ -1,27 +1,78 @@
 package br.labsec.thesis.otimization;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
 import br.labsec.thesis.polynomials.Trinomial;
 
 public class TrinomialContXor {
 
-	Trinomial trinomial;
-	
+	private Trinomial trinomial;
+	private HSSFWorkbook workbook;
+	private HSSFSheet sheet;
 	public TrinomialContXor(Trinomial trinomial) {
-		
+
 		this.trinomial = trinomial;
+		workbook = new HSSFWorkbook();
+		sheet = workbook.createSheet("Sheet_1");
 		this.optmize();
 	}
+
 	private void optmize() {
-		BigInteger max_size = (this.trinomial.degree().multiply(new BigInteger("2"))).subtract(new BigInteger("2"));
-		System.out.println(max_size.toString());
+		BigInteger max_size = (this.trinomial.degree().multiply(new BigInteger(
+				"2"))).subtract(new BigInteger("2"));
+		BigInteger i;
+		generateFirstROW(max_size);
 		
+		ArrayList<BigInteger> elements_pass = new ArrayList<BigInteger>();
+		
+		i = new BigInteger("0");
+		while (i.compareTo(this.trinomial.degree()) == -1) {
+			
+			
+			i = i.add(BigInteger.ONE);
+		}
+		
+		
+		saveXls();
 	}
-	
-	
-	
-	
-	
+
+	private void generateFirstROW(BigInteger max_size) {
+		Row row = sheet.createRow(0);
+		BigInteger i = new BigInteger("0");
+		
+		int cellnum = max_size.intValue();
+		BigInteger temp = max_size.add(BigInteger.ONE);
+		while(i.compareTo(temp) != 0)
+		{
+			Cell cell = row.createCell(cellnum--);
+			cell.setCellValue(i.intValue());
+			i = i.add(BigInteger.ONE);
+		}
+	}
+
+	private void saveXls() {
+		try {
+		    FileOutputStream out = 
+		            new FileOutputStream(new File("Teste.xls"));
+		    workbook.write(out);
+		    out.close();
+		    System.out.println("Excel written successfully..");
+		     
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
 
 }
